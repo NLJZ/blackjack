6325a42efa3176c302d8d402d693cbfc4765d810
NLJZ <johnson.nathan.lee@gmail.com>
2020-05-12 11:12:52 +0200

    added score check function still needs to change ace value to 1 if 11 would make player bust

--- src/scripts/score.js
+++ src/scripts/score.js
@@ -1,6 +1,32 @@
-let dealerScore;
-let playerScore;
+let dealerScore = 0;
+let playerScore = 0;
 const scoreUpdate = () => {
+  // let dealerScore = 0;
+  // let playerScore = 0;
   console.log(dealerHand);
   console.log(playerHand);
+  let dh = [];
+  let ph = [];
+  dealerHand.forEach(function (el) {
+    if (el.value == "A") {
+      dh.push(11);
+    } else if (el.value == "K" || el.value == "Q" || el.value == "J") {
+      dh.push(10);
+    } else {
+      dh.push(parseInt(el.value));
+    }
+  });
+  playerHand.forEach(function (el) {
+    if (el.value == "A") {
+      ph.push(11);
+    } else if (el.value == "K" || el.value == "Q" || el.value == "J") {
+      ph.push(10);
+    } else {
+      ph.push(parseInt(el.value));
+    }
+  });
+  dealerScore = dh.reduce((a, b) => a + b, 0);
+  playerScore = ph.reduce((a, b) => a + b, 0);
+  console.log(dealerScore);
+  console.log(playerScore);
 };
