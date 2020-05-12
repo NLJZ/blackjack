f9445da93da4b5a1f86617ca1451476e1c11736c
NLJZ <johnson.nathan.lee@gmail.com>
2020-05-12 18:53:29 +0200

    made code in hitMe.js more legible

--- src/scripts/hitMe.js
+++ src/scripts/hitMe.js
@@ -2,7 +2,12 @@ const hit = () => {
   let newCard = newDeck[0];
   let { suit, value } = newCard;
   playerHand.push(newDeck.shift(0));
-  let cardDiv = `<div class="card ${suit}"><span class=cardValueTop>${value}</span><span class="${suit}Shape"></span><span class=cardValueBottom>${value}</span></div>`;
+  let cardDiv = `
+  <div class="card ${suit}">
+  <span class=cardValueTop>${value}</span>
+  <span class="${suit}Shape"></span>
+  <span class=cardValueBottom>${value}</span>
+  </div>`;
   playerCards.innerHTML += cardDiv;
   scoreUpdate();
 };
