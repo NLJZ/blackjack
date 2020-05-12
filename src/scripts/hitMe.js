9aa236073993e189d71759e5c5b107ec3a20df5b
NLJZ <johnson.nathan.lee@gmail.com>
2020-05-12 15:29:31 +0200

    add hit button functionality

--- src/scripts/hitMe.js
+++ src/scripts/hitMe.js
@@ -1,3 +1,8 @@
 const hit = () => {
-  console.log(newDeck);
+  let newCard = newDeck[0];
+  let { suit, value } = newCard;
+  playerHand.push(newDeck.shift(0));
+  let cardDiv = `<div class="card ${suit}"><span class=cardValueTop>${value}</span><span class="${suit}Shape"></span><span class=cardValueBottom>${value}</span></div>`;
+  playerCards.innerHTML += cardDiv;
+  scoreUpdate();
 };
